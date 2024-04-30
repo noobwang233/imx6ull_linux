219c8ef48e12a2b7a4427e5f768d6dbe9bed3cd9
noobwang233 <1213236870@qq.com>
2024-04-30 11:08:27 +0800

    fix net

--- linux-imx_4.1.15/mx6ull_alientek_mini_emmc.sh
+++ linux-imx_4.1.15/mx6ull_alientek_mini_emmc.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_alientek_emmc_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_atk_mini_emmc_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j16
 
+rm ~/OSD_share/zImage
 cp arch/arm/boot/zImage ~/OSD_share
-cp arch/arm/boot/dts/imx6ull-alientek-emmc.dtb ~/OSD_share
+rm ~/OSD_share/imx6ull-atk-mini-emmc.dtb
+cp arch/arm/boot/dts/imx6ull-atk-mini-emmc.dtb ~/OSD_share
