68bd70a402c84db36b31b07c4d011936e7fafd05
noobwang233 <1213236870@qq.com>
2024-05-14 10:05:00 +0800

    init

--- linux-imx_4.1.15/mx6ull_alientek_emmc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_alientek_emmc_defconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j16
