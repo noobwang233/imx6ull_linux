7c7f6373a8e3cc2505ea765a41fec93a0ae5b9b6
noobwang233 <1213236870@qq.com>
2024-05-15 09:42:13 +0800

    save

--- linux-imx_4.1.15/mx6ull_alientek_mini_emmc.sh
+++ linux-imx_4.1.15/mx6ull_alientek_mini_emmc.sh
@@ -4,7 +4,7 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_atk_mini_emmc_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j16
 
-rm ~/OSD_share/zImage
-cp arch/arm/boot/zImage ~/OSD_share
-rm ~/OSD_share/imx6ull-atk-mini-emmc.dtb
-cp arch/arm/boot/dts/imx6ull-atk-mini-emmc.dtb ~/OSD_share
+rm ~/OSD_share/image/zImage
+cp arch/arm/boot/zImage ~/OSD_share/image
+rm ~/OSD_share/image/imx6ull-atk-mini-emmc.dtb
+cp arch/arm/boot/dts/imx6ull-atk-mini-emmc.dtb ~/OSD_share/image
